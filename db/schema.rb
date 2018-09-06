a3eb8c5b9a1ae3cdcdbd5e089014f78056d1d7f3
Diego Lorca Chacon <diego.lorca.chacon@gmail.com>
2018-09-06 17:31:37 -0300

    relation table event whit user and task okey

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180906180610) do
+ActiveRecord::Schema.define(version: 20180906202615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.boolean "completed", default: false
+    t.bigint "user_id"
+    t.bigint "task_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_events_on_task_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
@@ -36,4 +46,6 @@ ActiveRecord::Schema.define(version: 20180906180610) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "tasks"
+  add_foreign_key "events", "users"
 end
