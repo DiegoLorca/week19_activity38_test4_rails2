9744658c64980dc1a653294ef11870574e6d1a90
Diego Lorca Chacon <diego.lorca.chacon@gmail.com>
2018-09-06 16:11:56 -0300

    seed and navbar okey

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+tasks = Task.create([
+  { title: 'Comer una empanda', image:'https://www.enmicocinahoy.cl/wp-content/uploads/2008/09/empanada-pino-h-2.jpg' },
+  { title: 'Bailar una patita', image: 'http://www.radiozero.cl/static/2017/09/18--1200x500.jpg'  },
+  { title: 'Tomarse un Terremoto', image: 'https://amqueretaro.com/wp-content/uploads/2015/06/Las-bebidas-con-las-que-los-aficionadas-acompanan-la-Copa-America1-700x3251.jpg'  },
+  { title: 'Jugar una pichanga', image: 'https://i.ytimg.com/vi/ZjafJm4s77g/maxresdefault.jpg' },
+  {title: 'Ir a una fonda', image: 'https://media.biobiochile.cl/wp-content/uploads/2013/09/Paola-Williamson-C2-730x438.jpg' },
+  {title: 'Tratar de no curarse raja', image: 'https://i.ytimg.com/vi/R7SbP_tnqxs/hqdefault.jpg' },
+  {title: 'Jugar al emboque', image:'https://www.tvn.cl/incoming/ppaljpg-2486046/ALTERNATES/w1024h768/ppal.jpg' },
+  {title: 'Compartir en familia un asado', image: 'http://img.soy-chile.cl/Fotos/2015/04/03/file_20150403164701.jpg' },
+  {title: 'Estresarse en el trabajo', image: 'https://st3.depositphotos.com/13768208/18097/i/1600/depositphotos_180978044-stock-photo-stressed-businessman-in-office-scream.jpg' }])
