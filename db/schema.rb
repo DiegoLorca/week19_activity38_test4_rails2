719a96f614447006d8309960ead8d9e2b361b4f9
Diego Lorca Chacon <diego.lorca.chacon@gmail.com>
2018-09-06 21:46:24 -0300

    link show image, navbar user sign up, sign in and exit okey

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180906202615) do
+ActiveRecord::Schema.define(version: 20180906230129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20180906202615) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "done", default: false
   end
 
   create_table "users", force: :cascade do |t|
